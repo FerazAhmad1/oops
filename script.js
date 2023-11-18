f125c3dcedc7262e2a9e171f09f00b482ec9bb4e
Feraz ahmad <ferazahmadgit62@gmail.com>
2023-11-18 20:42:09 +0530

    a method is added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use-strict";
 
-const Person = function (firstName, lastName) {
+const Person = function (firstName, lastName, birthyear) {
   this.firstName = firstName;
   this.lastName = lastName;
+  this.birthyear = birthyear;
+  this.calcAge = () => {
+    return new Date().getFullYear() - this.birthyear;
+  };
 };
 
-const studentA = new Person("feraz", "ahmad");
+const studentA = new Person("feraz", "ahmad", 1995);
+console.log(studentA.calcAge());
 console.log(studentA);
