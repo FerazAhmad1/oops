b961b7e702c04e4d28bab9e5df84d3b75ba42c3c
Feraz ahmad <ferazahmadgit62@gmail.com>
2023-11-19 00:05:30 +0530

    method outside constructor function

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const Person = function (firstName, lastName, birthyear) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.birthyear = birthyear;
-  this.calcAge = () => {
-    return new Date().getFullYear() - this.birthyear;
-  };
 };
 
+Person.prototype.calcAge = function () {
+  console.log(this, "this");
+  return new Date().getFullYear() - this.birthyear;
+};
 const studentA = new Person("feraz", "ahmad", 1995);
 console.log(studentA.calcAge());
-console.log(studentA);
+console.log(studentA.__proto__); //  studentA.__proto__ is prototype of studentA,  Person.prototype is prototype property  of Person
+console.log(Person.prototype.isPrototypeOf(studentA), "check");
