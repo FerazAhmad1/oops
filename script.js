4612a791a5fdf09a5ee005aa01d67de43a4785a7
Feraz ahmad <ferazahmadgit62@gmail.com>
2023-11-18 20:24:30 +0530

    objet is created using constructor function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use-strict";
+
+const Person = function (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+
+const studentA = new Person("feraz", "ahmad");
+console.log(studentA);
